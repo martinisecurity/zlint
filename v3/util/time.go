f65e00f6fc622f6d8332b9655643084e530c331d
microshine <microshine@mail.ru>
2022-10-28 01:02:36 +0300

    fix(shaken): incorrect UnitedStatesSHAKENCP_Date

--- v3/util/time.go
+++ v3/util/time.go
@@ -77,12 +77,12 @@ var (
 	// If we apply the same rule of 90 days after approval for CAs to be conformant with new versions as is applied to CP->CPS
 	// changes this means any changes would be required for certificates issued on or after Thursday, December 3, 2020.
 	ATIS1000080_v003_Leaf_Date = time.Date(2020, time.December, 3, 0, 0, 0, 0, time.UTC)
-	ATIS1000080_v004_Date                            = time.Date(2021, time.October, 18, 0, 0, 0, 0, time.UTC)
+	ATIS1000080_v004_Date      = time.Date(2021, time.October, 18, 0, 0, 0, 0, time.UTC)
 	// ATIS-1000080.v004 was published on Monday, 18 October 2021.
 	// If we apply the same rule of 90 days after approval for CAs to be conformant with new versions as is applied to CP->CPS
 	// changes this means any changes would be required for certificates issued on or after Sunday, January 16, 2022.
 	ATIS1000080_v004_Leaf_Date = time.Date(2022, time.January, 16, 0, 0, 0, 0, time.UTC)
-	UnitedStatesSHAKENCP_Date                = time.Date(2022, time.August, 18, 0, 0, 0, 0, time.UTC)
+	UnitedStatesSHAKENCP_Date  = time.Date(2021, time.August, 18, 0, 0, 0, 0, time.UTC)
 	// CPS 1.3 was approved on August 18, 2021.
 	// If we assume that CAs CPSs were submitted for approval on the 45th day, the CPS was approved within 10 days
 	// and the CA has 90 days to become conformant then certificates issued on or after "Monday, January 10, 2022"
